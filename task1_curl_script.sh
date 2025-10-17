3d1853286e077ee9450cfef9279ed13683c9f45d
Anastasiia Sokolenko <sokolenko.anastasiia@gmail.com>
2025-10-17 13:22:04 +0200

    Updated the log file name

--- task1_curl_script.sh
+++ task1_curl_script.sh
@@ -7,12 +7,14 @@ for url in "${urls[@]}"
 do
   # виконуємо curl запит і отримуємо HTTP статус-код, -L слідує за редиректами, -s робить запит тихим, -o /dev/null відкидає вивід
   http_status=$(curl -s -L -o /dev/null -w "%{http_code}" "$url")
-  # перевірка HTTP статус-коду, результат перевірки записаний у файл логів curl_log.txt
+
+  # перевірка HTTP статус-коду, результат перевірки записаний у файл логів website_status.log
   if [ "$http_status" -eq 200 ]; then
-    echo "$(date): $url is UP" >> curl_log.txt
+    echo "$(date): $url is UP" >> website_status.log
   else
-    echo "$(date): $url is DOWN (HTTP $http_status)" >> curl_log.txt
+    echo "$(date): $url is DOWN (HTTP $http_status)" >> website_status.log
   fi
 done
+
 # вивід інформації про завершення скрипту
-echo "Результати перевірки доступності вебсайтів записано у файл curl_log.txt"
+echo "Результати перевірки доступності вебсайтів записано у файл website_status.log"
